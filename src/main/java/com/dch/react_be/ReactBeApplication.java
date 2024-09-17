c91875c53397fe662e07236a2e1ddc0d8313e78d
Dariusz Chmiela <dariusz.chmiela@ipfdigital.com>
2024-09-17 11:29:36 +0200

    Initial commit

---
+++ src/main/java/com/dch/react_be/ReactBeApplication.java
@@ -0,0 +1,13 @@
+package com.dch.react_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dch/react_be/ReactBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dch.react_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
